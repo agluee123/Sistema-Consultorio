941d1ea423748e49d83812d74f8a7131b9424de4
Daiana-g <daiana.g.97@hotmail.com>
2025-01-06 21:30:33 -0300

    Modificación

--- Capa Presentacion/VistaMedico.cs
+++ Capa Presentacion/VistaMedico.cs
@@ -108,24 +108,24 @@ namespace Capa_Presentacion
 
         public static void DiseñoDgv(ref DataGridView dgv)
         {
-            // General
-            dgv.EnableHeadersVisualStyles = false; // Deshabilita el estilo visual predeterminado
+          
+            dgv.EnableHeadersVisualStyles = false; 
             dgv.BorderStyle = BorderStyle.None;
             dgv.CellBorderStyle = DataGridViewCellBorderStyle.None;
             dgv.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
-            dgv.RowHeadersVisible = false; // Oculta los encabezados de fila
-            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect; // Selección por fila completa
-            dgv.MultiSelect = false; // Deshabilita la selección múltiple
+            dgv.RowHeadersVisible = false; 
+            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv.MultiSelect = false; 
 
-            // Estilo de selección en encabezados
-            dgv.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.Transparent; // Sin cambio de color al seleccionar encabezado
-            dgv.ColumnHeadersDefaultCellStyle.SelectionForeColor = dgv.ColumnHeadersDefaultCellStyle.ForeColor; // Sin cambio de texto al seleccionar encabezado
+           
+            dgv.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.Transparent; 
+            dgv.ColumnHeadersDefaultCellStyle.SelectionForeColor = dgv.ColumnHeadersDefaultCellStyle.ForeColor; 
 
-            // Estilo de encabezados
-            dgv.ColumnHeadersDefaultCellStyle.BackColor = Color.Black; // Fondo negro
-            dgv.ColumnHeadersDefaultCellStyle.ForeColor = Color.White; // Letras blancas
-            dgv.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10F, FontStyle.Bold); // Fuente en negrita
-            dgv.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter; // Alineación al centro
+           
+            dgv.ColumnHeadersDefaultCellStyle.BackColor = Color.Black; 
+            dgv.ColumnHeadersDefaultCellStyle.ForeColor = Color.White; 
+            dgv.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10F, FontStyle.Bold); 
+            dgv.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter; 
         }
 
         
